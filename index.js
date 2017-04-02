21739afbde34274316cc0147a81d72b4c03bfb26
jfkbeatkid <hernadezrigoberto@gmail.com>
2017-04-02 12:10:51 -0400

    google calendar api calls authenticated and working

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
 require('dotenv').config();
 var express = require('express');
 var server = express();
-var bodyParser = require('body-parser');
 var cors = require('cors');
 var google = require('googleapis');
 var GoogleAuth = require('google-auth-library');
 var authFactory = new GoogleAuth();
-var dns = google.dns('v1');
-var config = require('./client_id');
 var calendarConfig = require('./config');
-// var OAuth2 = google.auth.OAuth2;
 var port = process.env.PORT || 3000;
-//
 
-
-var oauth2Client
+var authClient;
 
 authFactory.getApplicationDefault(function(err, authClient) {
   if (err) {
@@ -22,32 +16,37 @@ authFactory.getApplicationDefault(function(err, authClient) {
     return;
   }
   if (authClient.createScopedRequired && authClient.createScopedRequired()) {
-    var scopes = ['https://www.googleapis.com/auth/calendar'];
+    var scopes = [
+      'https://www.googleapis.com/auth/calendar',
+      'https://www.googleapis.com/auth/calendar.readonly'
+    ];
     authClient = authClient.createScoped(scopes);
   }
-  oauth2Client = authClient;
+
+  var calendar = google.calendar({
+    version: 'v3',
+    auth: authClient
+  })
+
+  server.get('/', function(req, res) {
+    getEvents(calendar, authClient);
+    res.send('MADE IT')
+  })
+
+  server.listen(port, function() {
+    console.log('NOW LISTENING ON PORT...', port);
+  })
 
 });
 
-var calendar = google.calendar({
-  version: 'v3',
-  auth: oauth2Client
-})
-
-function getEvents() {
+function getEvents(calendar, authClient) {
   calendar.events.list({
-    auth: oauth2Client,
+    auth: authClient,
     calendarId: calendarConfig.calendarId
   }, function( err, events ) {
     if(err) {
       console.log('ERROR IN GETTING EVENTS', err);
     }
-    console.log('EVENTS IN GET EVENTS', events);
+    console.log('EVENTS IN GET EVENTS', events.items);
   })
 }
-
-server.listen(port, function() {
-  console.log('NOW LISTENING ON PORT...', port);
-  console.log('Google api', getEvents());
-  // console.log('OAUTH2CLIENT:', authFactory, 'GOOGLE:',  google);
-})
